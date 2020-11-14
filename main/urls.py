b1927cbee1e17747dddcea8562f179f8bc200f1b
Harish <harishpoctor@gmail.com>
2020-11-14 18:48:12 +0530

    removing error caused by not including styles url in urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ app_name = 'main'  # here for namespacing of urls.
 urlpatterns = [
     path("", views.homepage, name="homepage"),
 	path("submit_mas_entry", views.submit_mas_entry, name="submit_mas_entry"),
+	path("mystyles", views.mystyles, name="mystyles"),
 ]
